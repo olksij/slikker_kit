1cecef12c02778172f59a0a469c0e08f46df3cab
alexey <pixel.coder7@gmail.com>
2021-07-13 14:14:37 +0300

    Fixed wrong animation state

--- lib/src/animations.dart
+++ lib/src/animations.dart
@@ -3,7 +3,7 @@ import 'dart:math';
 import 'package:flutter/widgets.dart';
 
 /// Elastic-based curve.
-/// - [direction] choose between in curve and out curve.
+/// - [period] is the duration of the oscillation.
 /// - [smthns] decides how smooth animation is. Highes values means bigger amplitude.
 class SlikkerCurve extends ElasticOutCurve {
   /// The duration of the oscillation.
@@ -12,26 +12,28 @@ class SlikkerCurve extends ElasticOutCurve {
   /// Decides how smooth animation is. Highes values means bigger amplitude.
   final double smthns;
 
-  final bool forward;
+  final bool _forward;
 
   SlikkerCurve({
     this.smthns = 8,
     this.period = 0.6,
-  }) : forward = true;
+  }) : _forward = true;
 
   SlikkerCurve.reverse({
     this.smthns = 8,
     this.period = 0.6,
-  }) : forward = false;
+  }) : _forward = false;
 
   @override
   double transformInternal(double t) {
-    if (!forward) t = 1 - t;
+    if (!_forward) t = 1 - t;
     final num base = pow(2, -smthns * t);
     final double curve = sin((t - period / 4) * (pi * 2) / period);
-    return forward ? base * curve + 1 : -base * curve;
+    return _forward ? base * curve + 1 : -base * curve;
   }
 }
+
+enum _Direction { forward, reverse }
 
 /// A controller with an applied curve for an animation
 class SlikkerAnimationController {
@@ -76,6 +78,8 @@ class SlikkerAnimationController {
   /// Release the resources used by this object.
   void dispose() => controller.dispose();
 
+  _Direction _direction = _Direction.forward;
+
   /// Starts running this animation till the end. [forward] decides
   /// direction of the animation.
   ///
@@ -90,9 +94,13 @@ class SlikkerAnimationController {
     double tillEnd = forward ? controller.value : 1 - controller.value;
     int wait = tillEnd * controller.duration!.inMilliseconds ~/ 1;
 
+    _direction = forward ? _Direction.forward : _Direction.reverse;
+
     Future.delayed(
       Duration(milliseconds: wait),
       () {
+        if ((this._direction == _Direction.forward && !forward) ||
+            (this._direction == _Direction.reverse && forward)) return;
         animation.curve = forward ? curve : reverseCurve ?? curve.flipped;
         controller.duration = duration;
         forward ? controller.forward(from: 0) : controller.reverse(from: 1);
