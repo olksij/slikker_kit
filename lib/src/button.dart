f420b585c052a95e713056813906c567babd869f
alexey <pixel.coder7@gmail.com>
2021-07-16 12:28:06 +0300

    Integrated Slikkerbutton into effect painter.

--- lib/src/button.dart
+++ lib/src/button.dart
@@ -23,7 +23,7 @@ class SlikkerButton extends StatefulWidget {
   final bool disabled;
 
   /// If non-null, the corners of this box are rounded by this [BorderRadiusGeometry] value.
-  final BorderRadius borderRadius;
+  final BorderRadius? borderRadius;
 
   /// The widget below this widget in the tree.
   final Widget? child;
@@ -31,13 +31,8 @@ class SlikkerButton extends StatefulWidget {
   /// The empty space that surrounds the card inside.
   final EdgeInsetsGeometry? padding;
 
-  /// The empty space that surrounds the card inside.
-  final EdgeInsetsGeometry? margin;
-
   /// The [Function] that will be invoked on user's tap.
-  final Function onTap;
-
-  static function() {}
+  final Function? onTap;
 
   @override
   _SlikkerButtonState createState() => _SlikkerButtonState();
@@ -47,11 +42,10 @@ class SlikkerButton extends StatefulWidget {
     this.accent = 240,
     this.minor = false,
     this.child,
-    this.onTap = function,
+    this.onTap,
     this.borderRadius = const BorderRadius.all(Radius.circular(12)),
     this.disabled = false,
     this.padding,
-    this.margin,
   }) : super(key: key);
 }
 
@@ -88,21 +82,26 @@ class _SlikkerButtonState extends State<SlikkerButton>
     disabledAnmt.dispose();
     hoverAnmt.dispose();
     minorAnmt.dispose();
+    pressAnmt.dispose();
     super.dispose();
   }
 
-  void hover(bool state) {
-    if (widget.disabled) return;
-    hoverAnmt.run(state);
+  /// Number ranging from 0.0 to 1.0, where 1.0 means that element is elevated.
+  /// Elevation represents button's state.
+  double get elevation {
+    if (hoverAnmt.controller.isAnimating || hoverAnmt.value > 0)
+      return hoverAnmt.value - pressAnmt.value * hoverAnmt.value * 0.75;
+    return (1 - hoverAnmt.value) * pressAnmt.value;
   }
 
-  void touchEvent({TapDownDetails? tapDown, TapUpDetails? tapUp}) {
+  /// Button's [BorderRadius] based on [elevation].
+  BorderRadius get borderRadius => BorderRadius.lerp(
+      widget.borderRadius, BorderRadius.circular(16), elevation)!;
+
+  void _touchEvent({TapDownDetails? tapDown, TapUpDetails? tapUp}) {
     if (widget.disabled) return;
-
     if (tapDown != null) HapticFeedback.lightImpact();
-
     pressAnmt.run(tapDown != null);
-
     tapPosition = TapPosition(
       tapDown?.localPosition.dx ?? tapUp!.localPosition.dx,
       tapDown?.localPosition.dy ?? tapUp!.localPosition.dy,
@@ -110,22 +109,14 @@ class _SlikkerButtonState extends State<SlikkerButton>
   }
 
   Widget _buildButton(context, child) {
-    Widget button = child;
-    late double elevation;
+    Widget button =
+        Padding(padding: widget.padding ?? EdgeInsets.zero, child: child);
 
-    if (hoverAnmt.controller.isAnimating || hoverAnmt.value > 0)
-      elevation = hoverAnmt.value - pressAnmt.value * hoverAnmt.value * 0.75;
-    else
-      elevation = (1 - hoverAnmt.value) * pressAnmt.value;
-
-    if (widget.padding != null)
-      button = Padding(padding: EdgeInsets.all(15), child: button);
-
-    if (!widget.disabled)
+    if (!widget.disabled && widget.onTap != null)
       button = GestureDetector(
-        onTapDown: (details) => touchEvent(tapDown: details),
-        onTapUp: (details) => touchEvent(tapUp: details),
-        onTap: () => widget.onTap(),
+        onTapDown: (details) => _touchEvent(tapDown: details),
+        onTapUp: (details) => _touchEvent(tapUp: details),
+        onTap: () => widget.onTap!(),
         child: MouseRegion(
           onEnter: (event) => hoverAnmt.run(true),
           onExit: (event) => hoverAnmt.run(false),
@@ -133,77 +124,55 @@ class _SlikkerButtonState extends State<SlikkerButton>
         ),
       );
 
-    button = CustomPaint(
-      painter: _ButtonEffects(
-        accent: widget.accent,
-        tapPosition: tapPosition,
-        pressCtrl: pressAnmt,
-        borderRadius: BorderRadius.lerp(
-          widget.borderRadius,
-          BorderRadius.circular(16),
-          elevation,
-        )!,
-        elevation: elevation,
-      ),
-      child: button,
-    );
-
     return Transform(
       alignment: Alignment.center,
-      transform: Matrix4.identity()
-        //..setEntry(3, 2, 0.001)
-        //..rotateY(lerp(0, tapPosition.dx / 2, pressAnmt.value) ?? 0)
-        //..rotateX(lerp(0, tapPosition.dy / 2, pressAnmt.value) ?? 0)
-        ..scale(lerpDouble(1, 1.15, elevation)!),
-      child: button,
+      transform: Matrix4.identity()..scale(1 + .15 * elevation),
+      child: CustomPaint(
+        painter: _ButtonEffects(this),
+        child: button,
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
     return AnimatedBuilder(
+      child: widget.child,
+      builder: _buildButton,
       animation: Listenable.merge([
         disabledAnmt.animation,
         hoverAnmt.animation,
         minorAnmt.animation,
         pressAnmt.animation,
       ]),
-      child: widget.child,
-      builder: _buildButton,
     );
   }
 }
 
 class _ButtonEffects extends CustomPainter {
-  double accent;
-  BorderRadius borderRadius;
-  TapPosition? tapPosition;
-  SlikkerAnimationController pressCtrl;
-  double elevation;
+  _SlikkerButtonState button;
+  SlikkerButton widget;
 
-  _ButtonEffects({
-    required this.accent,
-    required this.borderRadius,
-    required this.tapPosition,
-    required this.pressCtrl,
-    required this.elevation,
-  }) : super();
+  _ButtonEffects(this.button)
+      : widget = button.widget,
+        super();
 
   @override
   void paint(Canvas canvas, Size size) {
-    canvas = paintBox(canvas: canvas, size: size, borderRadius: borderRadius);
+    canvas = paintBox(canvas, size);
   }
 
-  Canvas paintBox({
-    required Canvas canvas,
-    required Size size,
-    required BorderRadius borderRadius,
-  }) {
-    final Paint paintLight = Paint()..color = Color(0x22FFFFFF);
+  Canvas paintBox(Canvas canvas, Size size) {
+    double accent = widget.accent;
+    BorderRadius borderRadius = button.borderRadius;
 
-    final paintBoxShadow = lerpDouble(.75, .65, elevation) ?? .6;
-    final Paint paintBox = Paint()
-      ..color = HSVColor.fromAHSV(paintBoxShadow, accent, .02, .99).toColor();
+    // Initializing Paint objects.
+
+    final paintLight = Paint()..color = Color(0x22FFFFFF);
+
+    final paintBoxA = lerpDouble(.75, .65, button.elevation)!;
+    final paintBox = Paint()
+      ..color = HSVColor.fromAHSV(paintBoxA, accent, .02, .99).toColor();
 
     final paintKeyShadow = Paint()
       ..color = HSVColor.fromAHSV(.25, accent, .05, .95).toColor();
@@ -211,6 +180,8 @@ class _ButtonEffects extends CustomPainter {
     final paintAmbientShadow = Paint()
       ..color = HSVColor.fromAHSV(.5, accent, .1, .97).toColor()
       ..maskFilter = MaskFilter.blur(BlurStyle.normal, 16);
+
+    // Top borders
 
     final double topBorder = max(
       borderRadius.topLeft.y,
@@ -222,6 +193,8 @@ class _ButtonEffects extends CustomPainter {
       borderRadius.bottomRight.y,
     );
 
+    // Box Base
+
     RRect boxBase(double top, double height, bool light, [bool all = false]) {
       return RRect.fromRectAndCorners(
         Rect.fromLTWH(0, top, size.width, height),
@@ -231,6 +204,8 @@ class _ButtonEffects extends CustomPainter {
         bottomRight: !light || all ? borderRadius.bottomRight : Radius.zero,
       );
     }
+
+    // Generate required paths
 
     final lightPath = Path.combine(
       PathOperation.difference,
@@ -244,6 +219,8 @@ class _ButtonEffects extends CustomPainter {
       Path()..addRRect(boxBase(_heightDelta + 3, bottomBorder, false)),
       Path()..addRRect(boxBase(_heightDelta, bottomBorder, false)),
     );
+
+    // Draw into canvas
 
     return canvas
       ..drawRRect(boxBase(4, size.height, true, true), paintAmbientShadow)
