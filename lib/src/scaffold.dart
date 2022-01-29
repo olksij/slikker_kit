4dc2b5799bfdb53e06396ee703b8dedca94027f2
Alexey Besida <pixel.coder7@gmail.com>
2022-01-29 18:09:32 +0200

    started moving to CustomMultiChildLayout

--- lib/src/scaffold.dart
+++ lib/src/scaffold.dart
@@ -1,9 +1,12 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:slikker_kit/slikker_kit.dart';
 
 import './theme.dart';
 import './top_button.dart';
+
+enum _Comps { navigation, actionButton, topButton, content, title, header, fab }
 
 /// Widget that helps to build a page.
 /// Full documentation will be later
@@ -40,55 +43,81 @@ class _SlikkerScaffoldState extends State<SlikkerScaffold> {
   Widget build(BuildContext context) {
     final theme = SlikkerTheme.of(context);
 
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        if (constraints.maxWidth > 640) {
-          Widget navigationBar = SizedBox(
-            width: 72,
-            child: ListView(
-              shrinkWrap: true,
-              children: const [
-                SlikkerButton(child: Text('woaa')),
-                SlikkerButton(child: Text('testtt')),
-              ],
-            ),
-          );
+    Map<_Comps, Widget?> relation = {
+      _Comps.navigation: const Text('nav'),
+      _Comps.actionButton: widget.actionButton,
+      _Comps.topButton: const Text('topButton'),
+      _Comps.content: widget.content,
+      _Comps.title: Text(widget.title ?? 'title'),
+      _Comps.header: widget.header,
+      _Comps.fab: widget.actionButton,
+    };
 
-          Widget titleBar = Container(
-            color: Color(0xFFDDFFFF),
-            height: 48,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text('super title'),
-                Align(
-                  child: widget.header ?? const SizedBox(),
-                  alignment: Alignment.center,
-                ),
-                widget.actionButton ?? const SizedBox(),
-              ],
-            ),
-          );
+    List<Widget> children = [];
 
-          Widget app = Row(
-            children: [
-              navigationBar,
-              const ColoredBox(
-                color: Color(0xFFFFFFFF),
-                child: Text('mm'),
-              ),
-            ],
-          );
+    relation.forEach((id, child) {
+      if (child != null) children.add(LayoutId(id: id, child: child));
+    });
 
-          // TODO: wallpaper adaptivity
-          return ColoredBox(
-            color: theme.backgroundColor,
-            child: Column(children: [titleBar, app]),
-          );
-        } else {
-          return const Text('mobile view to be redesigned');
-        }
-      },
+    return CustomMultiChildLayout(
+      delegate: _ScaffolderDelegate(),
+      children: children,
     );
+  }
+}
+
+class _ScaffolderDelegate extends MultiChildLayoutDelegate {
+  @override
+  void performLayout(Size size) {
+    // TODO: Detect low reachability interface
+    const bool highReach = true;
+
+    bool wideInterface = size.width > 480;
+
+    Size navSize = Size.zero;
+    Size titleSize = Size.zero;
+
+    if (hasChild(_Comps.navigation)) {
+      navSize = layoutChild(_Comps.navigation, BoxConstraints.loose(size));
+      positionChild(
+        _Comps.navigation,
+        Offset(
+          wideInterface ? 0 : 0 - navSize.width,
+          titleSize.height,
+        ),
+      );
+    }
+
+    if (hasChild(_Comps.actionButton)) {
+      positionChild(_Comps.actionButton, Offset.zero);
+      layoutChild(_Comps.actionButton, BoxConstraints.loose(size));
+    }
+    if (hasChild(_Comps.content)) {
+      positionChild(_Comps.content, Offset.zero);
+      layoutChild(_Comps.content, BoxConstraints.loose(size));
+    }
+    if (hasChild(_Comps.fab)) {
+      positionChild(_Comps.fab, Offset.zero);
+      layoutChild(_Comps.fab, BoxConstraints.loose(size));
+    }
+    if (hasChild(_Comps.header)) {
+      positionChild(_Comps.header, Offset.zero);
+      layoutChild(_Comps.header, BoxConstraints.loose(size));
+    }
+    if (hasChild(_Comps.title)) {
+      positionChild(_Comps.title, Offset.zero);
+      layoutChild(_Comps.title, BoxConstraints.loose(size));
+    }
+    if (hasChild(_Comps.topButton)) {
+      positionChild(_Comps.topButton, Offset.zero);
+      layoutChild(_Comps.topButton, BoxConstraints.loose(size));
+    }
+
+    return;
+  }
+
+  @override
+  bool shouldRelayout(covariant MultiChildLayoutDelegate oldDelegate) {
+    return false;
   }
 }
